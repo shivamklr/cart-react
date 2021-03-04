daaf0b27c0635f29df88679f3f3986d944fd31c4
Shivam Kalra <shivamkalra98@gmail.com>
2021-03-04 19:58:21 +0530

    Updates

---
+++ precache-manifest.f3f169b7feee910dfade70fcbfe3d50d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60ce033ac870a2e47e0e0f38f15a2f92",
+    "url": "/cart-react/index.html"
+  },
+  {
+    "revision": "5106dcae6624003535eb",
+    "url": "/cart-react/static/css/main.ac000ce3.chunk.css"
+  },
+  {
+    "revision": "2829f658d05344f4543e",
+    "url": "/cart-react/static/js/2.4904b4fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cart-react/static/js/2.4904b4fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5106dcae6624003535eb",
+    "url": "/cart-react/static/js/main.b7b5ef5d.chunk.js"
+  },
+  {
+    "revision": "33e71f27b6ac1b32ae61",
+    "url": "/cart-react/static/js/runtime-main.df0cd489.js"
+  }
+]);
